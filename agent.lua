4ceadd3a1c9a238418d32a546563739437c7acf9
AnTonyPapako <p18papa5@ionio.gr>
2023-01-07 11:54:28 +0200

    Create agent.lua

---
+++ agent.lua
@@ -0,0 +1,12 @@
+function Image(img)
+    if img.classes:find('agent',1) then
+      local f = io.open("contribution/" .. img.src, 'r')
+      local doc = pandoc.read(f:read('*a'))
+      f:close()
+      local caption = pandoc.utils.stringify(doc.meta.caption) 
+      local name = pandoc.utils.stringify(doc.meta.name)
+      local am = pandoc.utils.stringify(doc.meta.id)
+      local content = "> " .. caption .. "  \n>" .. "Student Name:" .. name .. "\nID:" .. am
+      return pandoc.RawInline('markdown',content)
+    end
+end
